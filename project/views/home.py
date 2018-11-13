bfcfc67b3927c988d7d4e8e257a043ed1edf7f5a
devhid <devhid@users.noreply.github.com>
2018-11-12 23:17:09 -0500

    Fixed bug with serving static files for home page.

--- project/views/home.py
+++ project/views/home.py
@@ -2,11 +2,6 @@ from flask import Blueprint, render_template, url_for, redirect
 
 blueprint = Blueprint('home', __name__, static_folder='../static')
 
-
 @blueprint.route('/home/')
 def home():
-    return redirect(url_for('home.browse'))
-
-@blueprint.route('/home/browse')
-def browse():
     return render_template('home.html')
--- project/views/signup.py
+++ project/views/signup.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 
-blueprint = Blueprint('signup', __name__, static_folder='../static')
+blueprint = Blueprint('signup', __name__, static_url_path='')
 
 @blueprint.route('/')
 def signup():
